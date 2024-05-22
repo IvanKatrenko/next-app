a4f7aec063b9e4d4778ef7081607492ffa91c969
berno_4444 <ivankatrenko1997@gmail.com>
2024-05-22 21:21:18 +0200

    feature/accordion__second

--- src/app/ui/PageContent.tsx
+++ src/app/ui/PageContent.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { Modal, Flash, Accordion, AccordionRow } from '@/ui';
-import styles from './style.module.scss';
-import { useState } from 'react';
+import { Modal, Flash, Accordion, AccordionRow } from "@/ui";
+import styles from "./style.module.scss";
+import { useState } from "react";
 
 export const PageContent = () => {
-  const [accordionId, setAccordionId] = useState<string>('');
+  const [accordionId, setAccordionId] = useState<string>("");
   const accordionClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     const btn = e.currentTarget as HTMLButtonElement;
     const id = btn.id;
-    setAccordionId(id && id !== accordionId ? id : '');
+    setAccordionId(id && id !== accordionId ? id : "");
   };
 
   const [flash, setFlash] = useState<boolean>(false);
@@ -48,27 +48,43 @@ export const PageContent = () => {
         <Accordion className={styles.accordion}>
           <AccordionRow
             id="a_1"
-            title="Pierwszy tytuł"
-            isOpen={accordionId === 'a_1'}
+            title="What is the difference between a UI and UX Designer?"
+            isOpen={accordionId === "a_1"}
             handleClick={accordionClickHandler}
           >
             Treść numer jeden
           </AccordionRow>
           <AccordionRow
             id="a_2"
-            title="Drugi tytuł"
-            isOpen={accordionId === 'a_2'}
+            title="How to become a UI designer?"
+            isOpen={accordionId === "a_2"}
             handleClick={accordionClickHandler}
           >
             Treść numer dwa
           </AccordionRow>
           <AccordionRow
             id="a_3"
-            title="Trzeci tytuł"
-            isOpen={accordionId === 'a_3'}
+            title="What is the best UI design tool?"
+            isOpen={accordionId === "a_3"}
             handleClick={accordionClickHandler}
           >
             Treść numer trzy
+          </AccordionRow>
+          <AccordionRow
+            id="a_4"
+            title="What is the best place to learn Figma?"
+            isOpen={accordionId === "a_4"}
+            handleClick={accordionClickHandler}
+          >
+            Treść numer four
+          </AccordionRow>
+          <AccordionRow
+            id="a_5"
+            title="Should designers code?"
+            isOpen={accordionId === "a_5"}
+            handleClick={accordionClickHandler}
+          >
+            Treść numer five
           </AccordionRow>
         </Accordion>
       </section>
